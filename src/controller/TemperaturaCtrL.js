a397f4285375024b94507f251cd833ec7d15a0a7
renatobh83 <neo_rlm@hotmail.com>
2021-01-17 19:05:14 -0300

    maxMin

--- src/controller/TemperaturaCtrL.js
+++ src/controller/TemperaturaCtrL.js
@@ -2,25 +2,40 @@ const Temperaturas = require("../models/temperatura")
 const moment = require("moment")
 
 
-
-	function newDateString(days) {
-        const date =moment(days).format("DD/M HH:mm");
-      
-			return date
-		}
+let temperatura = 0.0
+let high = 0.0
+let maximaTemp = ''
+let atual = ''
+let low = ''
+let minTemp = ''
+function newDateString(days) {
+const date =moment(days).format("DD/M HH:mm");
+    return date
+}
 class TemperaturaController {
     async findAll(req, res){
-        let limit = 25
+        let limit = 15
         if(req.params.view) limit = parseInt(req.params.view)
-        const temperaturas = await Temperaturas.find({}, null, {sort: {data:1}, limit, skip:10})
+        const temperaturas = await Temperaturas.find({}, null, {sort: {data:-1}, limit})
         // res.render("index", {Temp: temperaturas})
-    	const data =temperaturas.map(temp =>[
-         temp.data.DHT11.Temperature]
-         )
-         const timeFor =[]
+        temperaturas.map((temp,index) =>{
+            temperatura = parseFloat(temp.data.DHT11.Temperature)
+            if(temperatura > high) {
+                high = temperatura
+                maximaTemp = temp.data
+            } 
+            low = parseFloat(temperaturas[0].data.DHT11.Temperature)
+            if(parseFloat(temperatura) < low){
+                minTemp = temp.data
+                low = temperatura
+            }
+         
+        })
+    	const data =temperaturas.map(temp =>[ temp.data.DHT11.Temperature])
+  
         const time = temperaturas.map(a => newDateString(a.data.Time))
-      
-        res.render("index", {div: data, time:time})
+       
+        res.render("index", {div: data, time:time, max: maximaTemp, min: minTemp})
     }
     
     async viewFilter(req, res){
