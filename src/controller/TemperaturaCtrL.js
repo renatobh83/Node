bbe6dd3151c52a44b1f16cda85f02d93b080f85b
renatobh83 <neo_rlm@hotmail.com>
2021-01-17 11:30:47 -0300

    time

--- src/controller/TemperaturaCtrL.js
+++ src/controller/TemperaturaCtrL.js
@@ -10,9 +10,9 @@ const moment = require("moment")
 		}
 class TemperaturaController {
     async findAll(req, res){
-        let limit = 10
+        let limit = 25
         if(req.params.view) limit = parseInt(req.params.view)
-        const temperaturas = await Temperaturas.find({}, null, {sort: {data:1}, limit, skip:15})
+        const temperaturas = await Temperaturas.find({}, null, {sort: {data:1}, limit, skip:10})
         // res.render("index", {Temp: temperaturas})
     	const data =temperaturas.map(temp =>[
          temp.data.DHT11.Temperature]
