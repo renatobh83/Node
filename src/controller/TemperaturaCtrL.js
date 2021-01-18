026e29c36ebc2b2d26f68865cfb50d0485f21d97
renatobh83 <neo_rlm@hotmail.com>
2021-01-18 09:55:09 -0300

    changeMaxMin

--- src/controller/TemperaturaCtrL.js
+++ src/controller/TemperaturaCtrL.js
@@ -6,7 +6,7 @@ let temperatura = 0.0
 let high = 0.0
 let maximaTemp = ''
 let atual = ''
-let low = ''
+let low = 0.0
 let minTemp = ''
 function newDateString(days) {
 const date =moment(days).format("DD/M HH:mm");
@@ -14,22 +14,30 @@ const date =moment(days).format("DD/M HH:mm");
 }
 class TemperaturaController {
     async findAll(req, res){
-        let limit = 15
+        let limit =200
         if(req.params.view) limit = parseInt(req.params.view)
         const temperaturas = await Temperaturas.find({}, null, {sort: {data:-1}, limit})
         // res.render("index", {Temp: temperaturas})
-        temperaturas.map((temp,index) =>{
-            temperatura = parseFloat(temp.data.DHT11.Temperature)
-            if(temperatura > high) {
-                high = temperatura
-                maximaTemp = temp.data
-            } 
-            low = parseFloat(temperaturas[0].data.DHT11.Temperature)
-            if(parseFloat(temperatura) < low){
-                minTemp = temp.data
-                low = temperatura
+   
+        temperaturas.map((temp) =>{
+           if(high === 0.0){ 
+              high = parseFloat(temp.dadosMaxima.DHT11.Temperature)
+                   maximaTemp = temp.dadosMaxima
             }
-         
+           
+          
+           if(parseFloat(temp.dadosMaxima.DHT11.Temperature) > high) {
+                maximaTemp = temp.dadosMaxima
+                high = parseFloat(temp.dadosMaxima.DHT11.Temperature)
+            }
+           if(low === 0.0){ 
+              low = parseFloat(temp.dadosMinima.DHT11.Temperature)
+              minTemp = temp.dadosMinima
+            }
+           if(parseFloat(temp.dadosMinima.DHT11.Temperature) < low){ 
+                 minTemp = temp.dadosMinima
+                  low = parseFloat(temp.dadosMinima.DHT11.Temperature)
+           }
         })
     	const data =temperaturas.map(temp =>[ temp.data.DHT11.Temperature])
   
--- src/models/temperatura.js
+++ src/models/temperatura.js
@@ -11,6 +11,12 @@ const TemperaturaSchema = new mongoose.Schema({
     data:{
         type: Object
     },
+    dadosMaxima: {
+      type: Object
+    },
+     dadosMinima: {
+      type: Object
+    },
     createdAt: {
         type: Date,
         default: Date.now() - 3 * 60 * 60 * 1000,
